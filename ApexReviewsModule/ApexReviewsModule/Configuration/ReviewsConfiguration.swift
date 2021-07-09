e9a059005537b95cda80b097c10152dc2f28bf42
Olivier Rigault <rigaulto13@gmail.com>
2021-07-09 15:02:24 +0100

    Add placeholders for configuration

---
+++ ApexReviewsModule/ApexReviewsModule/Configuration/ReviewsConfiguration.swift
@@ -0,0 +1,8 @@
+//
+//  ReviewsConfiguration.swift
+//  ApexReviewsModule
+//
+//  Created by Olivier Rigault on 09/07/2021.
+//
+
+import Foundation
---
+++ ApexSearchModule/ApexSearchModule/Configuration/SearchConfiguration.swift
@@ -0,0 +1,8 @@
+//
+//  SearchConfiguration.swift
+//  ApexSearchModule
+//
+//  Created by Olivier Rigault on 09/07/2021.
+//
+
+import Foundation
