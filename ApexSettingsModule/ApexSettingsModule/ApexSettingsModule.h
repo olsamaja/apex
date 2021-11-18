ced81a11e83bcfcfd5dcd00d1da78c870c0b5538
Olivier Rigault <olivier.rigault@newday.co.uk>
2021-11-18 13:03:32 +0000

    Add settings module

---
+++ ApexSettingsModule/ApexSettingsModule/ApexSettingsModule.h
@@ -0,0 +1,18 @@
+//
+//  ApexSettingsModule.h
+//  ApexSettingsModule
+//
+//  Created by Olivier Rigault on 18/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ApexSettingsModule.
+FOUNDATION_EXPORT double ApexSettingsModuleVersionNumber;
+
+//! Project version string for ApexSettingsModule.
+FOUNDATION_EXPORT const unsigned char ApexSettingsModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApexSettingsModule/PublicHeader.h>
+
+
