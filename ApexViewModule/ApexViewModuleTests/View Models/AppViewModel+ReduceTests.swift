1acc254305ef68c75d61608b6263d7cb168693d2
Olivier Rigault <Olivier.Rigault@photobox.com>
2024-07-05 17:19:22 +0100

    Add tests for AppViewModel

--- ApexViewModule/ApexViewModuleTests/View Models/AppViewModel+ReduceTests.swift
+++ ApexViewModule/ApexViewModuleTests/View Models/AppViewModel+ReduceTests.swift
@@ -32,7 +32,7 @@ final class AppViewModel_ReduceTests: XCTestCase {
                                      userRatingCount: 12345678,
                                      releaseNotes: "some release notes",
                                      releaseDate: releaseDate)
-        static let header = SectionRowsModel(header: ContentRowModel(.text("header")),
+        static let detailsSection = SectionRowsModel(header: ContentRowModel(.text("header")),
                                          rows: [ContentRowModel(.details(DetailsRowModel(details: details)))])
         static let graph = SectionRowsModel(header: ContentRowModel(.text("graph")),
                                          rows: [])
@@ -43,10 +43,16 @@ final class AppViewModel_ReduceTests: XCTestCase {
                                               ContentRowModel(.review(ReviewRowModel(review: review2)))])
     }
     
+    func testEquatableStates() throws {
+        XCTAssertEqual(AppViewModel.State.loadingDetails, AppViewModel.State.loadingDetails)
+        XCTAssertEqual(AppViewModel.State.loadingNextReviews([]), AppViewModel.State.loadingNextReviews([Constants.detailsSection]))
+        XCTAssertNotEqual(AppViewModel.State.loadingDetails, AppViewModel.State.loadingNextReviews([]))
+    }
+    
     func testReduceIdle() throws {
         XCTAssertEqual(AppViewModel.reduce(.idle, .onAppear), .loadingDetails)
         
-        let events: [AppViewModel.Event] = [.onDetailsLoaded(Constants.header),
+        let events: [AppViewModel.Event] = [.onDetailsLoaded(Constants.detailsSection),
                                             .onFailedToLoadData(DataError.invalidRequest)]
         
         events.forEach { event in
@@ -56,14 +62,40 @@ final class AppViewModel_ReduceTests: XCTestCase {
     
     func testReduceLoadingDetails() throws {
         XCTAssertEqual(AppViewModel.reduce(.loadingDetails, .onAppear), .loadingDetails)
-        XCTAssertEqual(AppViewModel.reduce(.loadingDetails, .onDetailsLoaded(Constants.header)), .detailsLoaded(Constants.header))
+        XCTAssertEqual(AppViewModel.reduce(.loadingDetails, .onDetailsLoaded(Constants.detailsSection)), .detailsLoaded(Constants.detailsSection))
         XCTAssertEqual(AppViewModel.reduce(.loadingDetails, .onFailedToLoadData(DataError.invalidRequest)), .error(DataError.invalidRequest))
     }
     
     func testReduceDetailsLoaded() throws {
-        let state = AppViewModel.State.detailsLoaded(Constants.header)
+        let state = AppViewModel.State.detailsLoaded(Constants.detailsSection)
         let events: [AppViewModel.Event] = [.onAppear,
-                                            .onDetailsLoaded(Constants.header),
+                                            .onDetailsLoaded(Constants.detailsSection),
+                                            .onFailedToLoadData(DataError.invalidRequest)]
+        
+        events.forEach { event in
+            XCTAssertEqual(AppViewModel.reduce(state, event), state)
+        }
+
+        XCTAssertEqual(AppViewModel.reduce(.detailsLoaded(Constants.detailsSection), 
+            .onLoadingNextReviews([Constants.detailsSection])),
+            .loadingNextReviews([Constants.detailsSection]))
+    }
+
+    func testReduceLoadingNextReviews() throws {
+        let initialState = AppViewModel.State.loadingNextReviews([Constants.detailsSection])
+        XCTAssertEqual(AppViewModel.reduce(initialState, .onAppear), initialState)
+
+        XCTAssertEqual(AppViewModel.reduce(initialState, .onFailedToLoadData(DataError.invalidRequest)), .error(DataError.invalidRequest))
+
+        let sections = [Constants.detailsSection] + [Constants.rows]
+        XCTAssertEqual(AppViewModel.reduce(initialState, .onNextReviewsloaded(sections)), .nextReviewsLoaded(sections))
+    }
+
+    func testNextReviewsLoaded() throws {
+        let sections = [Constants.detailsSection] + [Constants.rows]
+        let state = AppViewModel.State.nextReviewsLoaded(sections)
+        let events: [AppViewModel.Event] = [.onAppear,
+                                            .onDetailsLoaded(Constants.detailsSection),
                                             .onFailedToLoadData(DataError.invalidRequest)]
         
         events.forEach { event in
@@ -74,7 +106,7 @@ final class AppViewModel_ReduceTests: XCTestCase {
     func testReduceError() throws {
         let state = AppViewModel.State.error(DataError.invalidRequest)
         let events: [AppViewModel.Event] = [.onAppear,
-                                            .onDetailsLoaded(Constants.header),
+                                            .onDetailsLoaded(Constants.detailsSection),
                                             .onFailedToLoadData(DataError.invalidRequest)]
         
         events.forEach { event in
